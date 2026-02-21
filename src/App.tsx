a8da45d3269188e6bf1a13a2d488d7a447538179
Artt Vincii <otong123321@gmail.com>
2026-02-22 05:13:48 +0700

    refactor: update API URL in .env and clean up routing structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,6 @@ import ProjectPage from "./pages/public/ProjectPage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomeLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="pricing" element={<PricingPage />} />
-        <Route path="login" element={<LoginPage />} />
-      </Route>
-
-      <Route path="/dashboard" element={<DashboardLayout />}>
-        <Route index element={<DashboardHome />} />
-        <Route path="edit-profile" element={<EditProfilePage />} />
-        <Route path="manage-projects" element={<ManageProjectsPage />} />
-      </Route>
-
       <Route path="/:username" element={<PublicLayout />}>
         <Route index element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
