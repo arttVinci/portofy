db43ff23929dc465a7e0157b9aa32d13dfa903bb
Artt Vincii <otong123321@gmail.com>
2026-02-27 02:12:53 +0700

    feat: update routing in App component and simplify HomeLayout by removing marketing sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import { Routes, Route } from "react-router-dom";
+
+// 1. Layouts
+import HomeLayout from "./layouts/HomeLayout";
 import PublicLayout from "./layouts/PublicLayout";
-import HomePage from "./pages/public/HomePage";
+
+// 2. Pages Marketing (Jualan)
+import MarketingHomePage from "./pages/home/HomePage";
+import FeaturedPage from "./pages/home/FeaturedPage";
+
+// 3. Pages Portfolio (Public)
+import PortfolioHomePage from "./pages/public/HomePage";
 import AboutPage from "./pages/public/AboutPage";
 import AchievementPage from "./pages/public/AchievementPage";
 import DetailProjectPage from "./pages/public/DetailProjectPage";
 import ContactPage from "./pages/public/ContactPage";
 import ProjectPage from "./pages/public/ProjectPage";
-import HomeLayout from "./layouts/HomeLayout";
-import FeaturedPage from "./pages/home/FeaturedPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeLayout />}>
-        <Route index element={<HomeLayout />} />
+        <Route index element={<MarketingHomePage />} />
+        <Route path="fitur" element={<FeaturedPage />} />
       </Route>
 
-      <Route path="fitur" element={<FeaturedPage />} />
-
       <Route path="/:username" element={<PublicLayout />}>
-        <Route index element={<HomePage />} />
+        <Route index element={<PortfolioHomePage />} />{" "}
         <Route path="about" element={<AboutPage />} />
         <Route path="achievements" element={<AchievementPage />} />
         <Route path="projects" element={<ProjectPage />} />
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,25 +1,12 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/navigation/Navbar";
-import CtaBannerSection from "../sections/marketing/CtaBannerSection";
-import FaqSection from "../sections/marketing/FaqSection";
-import Footer from "../sections/marketing/Footer";
-import HeroSection from "../sections/marketing/HeroSection";
-import HowItWorksSection from "../sections/marketing/HowItsWorkSection";
-import LivePreviewSection from "../sections/marketing/LivePreviewSection";
-import PricingSection from "../sections/marketing/PricingSection";
-import TemplateShowcase from "../sections/marketing/TemplateShowcase";
 
 export default function HomeLayout() {
   return (
     <>
       <Navbar />
-      <HeroSection />
-      <HowItWorksSection />
-      <TemplateShowcase />
-      <LivePreviewSection />
-      <PricingSection />
-      <FaqSection />
-      <CtaBannerSection />
-      <Footer />
+
+      <Outlet />
     </>
   );
 }
