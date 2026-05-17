3a50f3ee756eb884195c7bc8f8162e1e25fe610a
Artt Vincii <otong123321@gmail.com>
2026-05-17 15:33:26 +0700

    feat: add Google AI Studio client initialization using viper configuration

--- BE/internal/config/google_ai_studio.go
+++ BE/internal/config/google_ai_studio.go
@@ -12,10 +12,11 @@ import (
 func NewGoogleAiStudio(config *viper.Viper) *genai.Client {
 	ctx := context.Background()
 
-    client, err := genai.NewClient(ctx, option.WithAPIKey(config.GetString("google_ai_studio.api_key")))
-    if err != nil {
-        log.Fatal(err)
-    }	
+	client, err := genai.NewClient(ctx, option.WithAPIKey(config.GetString("google_ai_studio.api_key")))
+	if err != nil {
+		log.Printf("Failed to create Google AI Studio client: %s", config.GetString("google_ai_studio.api_key"))
+		log.Fatal(err)
+	}
 
-  return client
+	return client
 }
