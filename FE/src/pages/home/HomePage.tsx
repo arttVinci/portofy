73cc29c2278a623f89965575fee3b8ad1a10fa91
Artt Vincii <otong123321@gmail.com>
2026-05-21 20:51:46 +0700

    feat: implement dashboard sidebar, notification card, and landing page sections

--- FE/src/pages/home/HomePage.tsx
+++ FE/src/pages/home/HomePage.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/sections/marketing/HeroSection";
 import WhySection from "@/sections/marketing/WhySection";
 import HowItWorksSection from "@/sections/marketing/HowItWorksSection";
 // import CTASection from "@/sections/marketing/CTASection";
-import PremiumSection from "@/sections/marketing/PremiumSection";
+// import PremiumSection from "@/sections/marketing/PremiumSection";
 // import BlogSection from "@/sections/marketing/BlogSection";
 import FAQSection from "@/sections/marketing/FAQSection";
 import FooterSection from "@/sections/marketing/FooterSection";
@@ -14,7 +14,7 @@ export default function HomePage() {
       <WhySection />
       <HowItWorksSection />
       {/* <CTASection /> */}
-      <PremiumSection />
+      {/* <PremiumSection /> */}
       {/* <BlogSection /> */}
       <FAQSection />
       <FooterSection />
