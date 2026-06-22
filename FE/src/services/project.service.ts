3488aa9bf9c410005f237295ae9885a47601f4ff
Artt Vincii <otong123321@gmail.com>
2026-06-22 22:09:30 +0700

    feat: implement ProjectService for CRUD operations and media uploads

--- FE/src/services/project.service.ts
+++ FE/src/services/project.service.ts
@@ -1,4 +1,10 @@
-import type { CreateProjectRequest, ProjectResponse, UpdateProjectRequest, ApiResponse, BulkDeleteProjectRequest } from "@/@types";
+import type {
+  CreateProjectRequest,
+  ProjectResponse,
+  UpdateProjectRequest,
+  ApiResponse,
+  BulkDeleteProjectRequest,
+} from "@/@types";
 import type { AxiosResponse } from "axios";
 import apiClient from "@/api/apiClient";
 
@@ -6,41 +12,62 @@ class ProjectService {
   private readonly BASE_PATH = "/projects";
 
   async create(payload: CreateProjectRequest): Promise<ProjectResponse> {
-    const response: AxiosResponse<ApiResponse<ProjectResponse>> = await apiClient.post(this.BASE_PATH, payload);
+    const response: AxiosResponse<ApiResponse<ProjectResponse>> =
+      await apiClient.post(this.BASE_PATH, payload);
     return response.data.data;
   }
 
-  async update(id: string, payload: UpdateProjectRequest): Promise<ProjectResponse> {
-    const response: AxiosResponse<ApiResponse<ProjectResponse>> = await apiClient.put(`${this.BASE_PATH}/${id}`, payload);
+  async update(
+    id: string,
+    payload: UpdateProjectRequest,
+  ): Promise<ProjectResponse> {
+    const response: AxiosResponse<ApiResponse<ProjectResponse>> =
+      await apiClient.put(`${this.BASE_PATH}/${id}`, payload);
     return response.data.data;
   }
 
   async delete(id: string): Promise<boolean> {
-    const response: AxiosResponse<ApiResponse<boolean>> = await apiClient.delete(`${this.BASE_PATH}/${id}`);
+    const response: AxiosResponse<ApiResponse<boolean>> =
+      await apiClient.delete(`${this.BASE_PATH}/${id}`);
     return response.data.data;
   }
 
   async bulkDelete(payload: BulkDeleteProjectRequest): Promise<boolean> {
-    const response: AxiosResponse<ApiResponse<boolean>> = await apiClient.delete(`${this.BASE_PATH}/_bulk`, {
-      data: payload,
-    });
+    const response: AxiosResponse<ApiResponse<boolean>> =
+      await apiClient.delete(`${this.BASE_PATH}/_bulk`, {
+        data: payload,
+      });
     return response.data.data;
   }
 
   async get(): Promise<ProjectResponse[]> {
-    const response: AxiosResponse<ApiResponse<ProjectResponse[]>> = await apiClient.get(this.BASE_PATH);
+    const response: AxiosResponse<ApiResponse<ProjectResponse[]>> =
+      await apiClient.get(this.BASE_PATH);
     return response.data.data;
   }
 
-  async uploadImage(payload: FormData): Promise<string> {
+  async uploadThumbnail(payload: FormData): Promise<string> {
     const response: AxiosResponse<ApiResponse<string>> = await apiClient.post(
-      `${this.BASE_PATH}/_image`,
+      `${this.BASE_PATH}/_thumbnail`,
       payload,
       {
         headers: {
           "Content-Type": "multipart/form-data",
         },
-      }
+      },
+    );
+    return response.data.data;
+  }
+
+  async uploadGallery(payload: FormData): Promise<string[]> {
+    const response: AxiosResponse<ApiResponse<string[]>> = await apiClient.post(
+      `${this.BASE_PATH}/_gallery`,
+      payload,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
     );
     return response.data.data;
   }
